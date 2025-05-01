e0389d3bde123837af928fd1c30211b4bde9807f
Jakob W Johnson <jake@Jakobs-MacBook-Pro.local>
2025-05-01 08:03:26 -0500

    comitting jump up update for mobile and desktop

--- jumpup/__settings__.js
+++ jumpup/__settings__.js
@@ -1,21 +1,21 @@
-ASSET_PREFIX = "";
-SCRIPT_PREFIX = "";
-SCENE_PATH = "1204680.json";
-CONTEXT_OPTIONS = {
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1204680.json";
+window.CONTEXT_OPTIONS = {
     'antialias': false,
     'alpha': false,
     'preserveDrawingBuffer': false,
-    'preferWebGl2': true,
+    'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-SCRIPTS = [ 52904789, 52908832, 52910126, 52915684, 52915685, 52915699, 53062003, 53757044, 53934998, 54295006, 54710759, 54710760, 54710764, 54713724, 54715226, 54927708, 54928178, 54995184, 55073762, 55073764, 55074598 ];
-CONFIG_FILENAME = "config.json";
-INPUT_SETTINGS = {
+window.SCRIPTS = [ 52908832, 52910126, 52915684, 52915685, 52915699, 53062003, 53757044, 53934998, 54295006, 54710759, 54710760, 54710764, 54713724, 54715226, 54927708, 54928178, 54995184, 55073762, 55073764, 55074598, 225721555 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
     useGamepads: false,
     useTouch: true
 };
 pc.script.legacy = false;
-PRELOAD_MODULES = [
+window.PRELOAD_MODULES = [
 ];
