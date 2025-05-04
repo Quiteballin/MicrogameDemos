c5e1658da787017a5056d6e6528ff363d63f7918
Jakob W Johnson <jake@Jakobs-MacBook-Pro.local>
2025-05-04 16:03:06 -0500

    updating 3pt with desktop ctrls and no prize link

--- 3ptcontest/__settings__.js
+++ 3ptcontest/__settings__.js
@@ -1,21 +1,21 @@
-ASSET_PREFIX = "";
-SCRIPT_PREFIX = "";
-SCENE_PATH = "1243781.json";
-CONTEXT_OPTIONS = {
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1243781.json";
+window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
     'preserveDrawingBuffer': false,
-    'preferWebGl2': true,
+    'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-SCRIPTS = [ 56683254, 56683466, 61974517, 62185564, 62221554, 62503951, 62849996, 64471189, 64479974, 64591321, 64655093, 64655347, 64745357, 64745454, 65325891, 65406028, 65406717, 65407199, 65900221, 65927382, 65927688, 66599217, 66614333, 67095781, 67455246 ];
-CONFIG_FILENAME = "config.json";
-INPUT_SETTINGS = {
+window.SCRIPTS = [ 174138226, 56683466, 61974517, 62185564, 62221554, 62503951, 62849996, 64471189, 64479974, 64591321, 64655093, 64655347, 64745357, 64745454, 65325891, 65406028, 65406717, 65407199, 65900221, 65927382, 65927688, 66599217, 66614333, 67095781, 67455246, 175515964 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
     useGamepads: false,
     useTouch: true
 };
 pc.script.legacy = false;
-PRELOAD_MODULES = [
+window.PRELOAD_MODULES = [
 ];
