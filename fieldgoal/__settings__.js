6847df1e7dbf348a8e500a549d88cd5b2a621dbc
Jakob W Johnson <jake@Jakobs-MacBook-Pro.local>
2025-05-05 07:40:42 -0500

    removing spaceship and fixing fieldgoal

--- fieldgoal/__settings__.js
+++ fieldgoal/__settings__.js
@@ -1,21 +1,21 @@
-ASSET_PREFIX = "";
-SCRIPT_PREFIX = "";
-SCENE_PATH = "1455551.json";
-CONTEXT_OPTIONS = {
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1455551.json";
+window.CONTEXT_OPTIONS = {
     'antialias': false,
     'alpha': true,
     'preserveDrawingBuffer': false,
-    'preferWebGl2': true,
+    'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-SCRIPTS = [ 90505937, 90505982, 90505977, 90505925, 90505927, 90505942, 90505968, 90505993, 90505984, 90505903, 90505923, 90936998, 92922730, 95322457, 95334011, 95868397, 90505914, 90663873, 96052815, 96235698, 96400501, 96438772, 96438773, 97416715, 99867394 ];
-CONFIG_FILENAME = "config.json";
-INPUT_SETTINGS = {
+window.SCRIPTS = [ 90505937, 90505982, 90505977, 90505927, 90505942, 90505968, 90505993, 90505984, 90505903, 90505923, 90936998, 92922730, 95322457, 95334011, 95868397, 90505914, 90663873, 96052815, 96235698, 96400501, 96438772, 96438773, 97416715, 99867394, 206424741 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
     useGamepads: false,
     useTouch: true
 };
 pc.script.legacy = false;
-PRELOAD_MODULES = [
+window.PRELOAD_MODULES = [
 ];
